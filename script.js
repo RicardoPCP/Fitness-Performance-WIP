0ad23c6b1cac958f59836402af07f18a0ae68d4a
RicardoPCP <ricardopessoa04@gmail.com>
2026-06-04 19:41:06 -0300

    Reformulação

---
+++ script.js
@@ -0,0 +1,9 @@
+function sidebarMobile(){
+  const sidebar = document.querySelector('.mobile');
+  sidebar.style.display = 'flex';
+}
+
+function fecharSidebar(){
+  const sidebar = document.querySelector('.mobile');
+  sidebar.style.display = 'none';
+}
