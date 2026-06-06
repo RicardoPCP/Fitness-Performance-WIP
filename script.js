3831464611e4d258ac206ae5208cec62235f02e3
RicardoPCP <ricardopessoa04@gmail.com>
2026-06-05 22:21:20 -0300

    hover novo

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function fecharSidebar(){
   const sidebar = document.querySelector('.mobile');
   sidebar.style.display = 'none';
 }
+
+document.querySelectorAll('.mobile a').forEach(link => {
+    link.addEventListener('click', function(){
+        document.querySelectorAll('.mobile a').forEach(l => l.classList.remove('ativo'));
+        this.classList.add('ativo');
+    });
+});
